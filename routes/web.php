091f5f17011569aeafc149d67c00bb2a79672d3b
Ha-ji-me <hjmx3599@gmail.com>
2022-03-15 14:48:43 +0900

    テストファイル

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/home', function() {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/test', function(){
+    return view('test');
+});
